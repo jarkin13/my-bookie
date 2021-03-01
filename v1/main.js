eb14097035182fbebeecb621e2c9b2d963293e28
sam <sarkin271@gmail.com>
2021-02-28 19:31:32 -0500

    js add/remove functionality in bet slip

--- v1/main.js
+++ v1/main.js
@@ -29,27 +29,50 @@
     };
   }
 
+  // Grabbing info from all bet-containers
   const allBets = document.querySelectorAll(".bet-container");
+  // Want to find empty bet container
+  const slip = document.getElementById("emptySlip");
   const userBets = [];
+
+  // Looping through allBets
   for (let i = 0; i < allBets.length; i++) {
     const bet = allBets[i];
+    //when we click bet runs function
     bet.onclick = function betSlip() {
+      // Setting bet information variables for specific bet clicked
       const title = bet.querySelector(".title");
       const odds = bet.querySelector(".odds");
       const id = bet.getAttribute("id");
-      let addBet = true; //need to add
+      // When true need to add bet to userBets[]
+      let addBet = true;
+      // Looping through userBets[], which is what has been clicked
       for (let j = 0; j < userBets.length; j++) {
         const userBet = userBets[j];
+        // If bet is already in userBets[] need to remove
         if (id === userBet) {
           addBet = false;
         }
       }
       if (addBet) {
-        //add bet to userBets
+        // When addBet is true add to userBets[]
         userBets.push(id);
+        // Want to copy empty container
+        let slipCopy = slip.cloneNode(true);
+        // Want to fill empty container with correct content with variables set above
+        slipCopy.querySelector(".title").innerHTML = title.innerHTML;
+        slipCopy.querySelector(".slip__total").innerHTML = odds.innerHTML;
+        // Change id of slip copy
+        slipCopy.setAttribute("id", id);
+        // Append item to .bet-slips
+        document.querySelector(".bet-slips").appendChild(slipCopy);
       } else {
-        //remove bet from userBets
+        // When addBet is false remove from userBets[]
         userBets.splice(userBets.indexOf(id), 1);
+        // Searching through .bet-slips
+        const betSlips = document.querySelector(".bet-slips");
+        // Of those bet-slips find id and remove it
+        betSlips.querySelector(`#${id}`).remove();
       }
     };
   }
