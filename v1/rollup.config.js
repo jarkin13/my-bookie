659dff2e55d736124a0da955d6b8c5074e7eaf18
sam <sarkin271@gmail.com>
2021-03-18 14:38:06 -0400

    moved postcss() into export default

--- v1/rollup.config.js
+++ v1/rollup.config.js
@@ -1,17 +1,16 @@
 import postcss from "rollup-plugin-postcss";
 import postcssImport from "postcss-import";
 
-postcss({
-  plugins: [postcssImport({})],
-  extract: true,
-  sourceMap: true,
-});
-
 export default {
   input: "src/main.js",
   output: {
     file: "dist/bundle.js",
     format: "cjs",
+    plugins: [
+      postcss({
+        plugins: [postcssImport()],
+        extract: true,
+      }),
+    ],
   },
-  plugins,
 };
