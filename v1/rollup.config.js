ef2500d18d0096879bb5f4cc3d0e8f2da90cb3cf
sam <sarkin271@gmail.com>
2021-03-18 14:34:04 -0400

    tried a few other alternatives but still getting many errors

--- v1/rollup.config.js
+++ v1/rollup.config.js
@@ -1,21 +1,17 @@
 import postcss from "rollup-plugin-postcss";
 import postcssImport from "postcss-import";
 
+postcss({
+  plugins: [postcssImport({})],
+  extract: true,
+  sourceMap: true,
+});
+
 export default {
   input: "src/main.js",
   output: {
     file: "dist/bundle.js",
     format: "cjs",
   },
+  plugins,
 };
-
-postcss({
-  extract: true,
-  extract: "dist/bundle.js",
-});
-
-postcss().use(
-  postcssImport({
-    path: ["src/styles.css"],
-  })
-);
