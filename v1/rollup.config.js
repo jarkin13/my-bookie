99b08574ba771d4429d82d50ca7b0e6c7372b405
sam <sarkin271@gmail.com>
2021-03-19 19:25:44 -0400

    rollup copy asset and html to dist

--- v1/rollup.config.js
+++ v1/rollup.config.js
@@ -1,5 +1,7 @@
 import postcss from "rollup-plugin-postcss";
 import postcssImport from "postcss-import";
+import browserSync from "rollup-plugin-browsersync";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/main.js",
@@ -13,6 +15,15 @@ export default {
       plugins: [postcssImport()],
       extract: true,
       minimize: true,
+    }),
+    browserSync({
+      server: "dist",
+    }),
+    copy({
+      targets: [
+        { src: "src/index.html", dest: "dist" },
+        { src: "src/assets/**/*", dest: "dist/assets" },
+      ],
     }),
   ],
 };
